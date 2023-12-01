6ca9b08b752f3f8623e83bb773349304b443731b
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2023-12-01 19:14:50 +0000

    test: add command test

---
+++ src/test/kotlin/dev/myugen/rover/CommandsShould.kt
@@ -0,0 +1,10 @@
+package dev.myugen.rover
+
+import org.junit.jupiter.api.Test
+
+class CommandsShould {
+    @Test
+    fun `calculate a new path from given current location`() {
+        
+    }
+}
