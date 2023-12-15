d6ce6f7bd30e165f9b8e1baf2dbdd937c67b14f2
Alberto Morales <kodenix@gmail.com>
2023-12-15 14:56:23 +0100

    Implemented turning right calculation

--- src/main/kotlin/dev/myugen/rover/Command.kt
+++ src/main/kotlin/dev/myugen/rover/Command.kt
@@ -1,10 +1,15 @@
 package dev.myugen.rover
 
+import dev.myugen.direction.East
 import dev.myugen.geography.Location
 import dev.myugen.geography.Path
+import dev.myugen.geography.Point
 
 data class Command(private val value: String) {
     fun calculatePathOver(currentLocation: Location): Path {
-        TODO("Not yet implemented")
+        if (value=="R") {
+            return  listOf(currentLocation.copy(direction = currentLocation.direction.turnRight))
+        }
+        throw RuntimeException("Invalid command")
     }
 }
