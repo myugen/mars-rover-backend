092695cc4b9622b2686ffca334ad2ac798a72bca
Alberto Morales <kodenix@gmail.com>
2023-12-28 16:08:59 +0100

    Added explicit planet size as param

--- src/main/kotlin/dev/myugen/rover/Rover.kt
+++ src/main/kotlin/dev/myugen/rover/Rover.kt
@@ -5,7 +5,7 @@ import dev.myugen.geography.Path
 
 class Rover private constructor(private val initialLocation: Location, private val planetSize: PlanetSize = PlanetSize(10,  10)) {
     fun execute(command: Command): Unit {
-        val path: Path = command.calculatePathOver(currentLocation())
+        val path: Path = command.calculatePathOver(currentLocation(), planetSize)
     }
 
     fun currentLocation(): Location = TODO("Current location not implemented")
