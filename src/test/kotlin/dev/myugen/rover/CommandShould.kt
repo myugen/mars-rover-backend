2926130c0504c137ec4753a520cb0eca5b9e2b7f
Miguel Ignacio Cabrera San Gil <miguelcabsan@gmail.com>
2024-01-05 14:25:49 +0000

    test: separate context for edges cases

--- src/test/kotlin/dev/myugen/rover/CommandShould.kt
+++ src/test/kotlin/dev/myugen/rover/CommandShould.kt
@@ -8,6 +8,8 @@ import dev.myugen.geography.Location
 import dev.myugen.geography.Path
 import dev.myugen.geography.Point
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.extension.ExtensionContext
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
@@ -47,9 +49,24 @@ class CommandCalculationProviderAlt : ArgumentsProvider {
             Arguments.of(listOf(Location(Point(1, 1), East)), Location(Point(2, 1), East), "B", "go backwards"),
             Arguments.of(listOf(Location(Point(0, 1), South)), Location(Point(0, 0), South), "B", "go backwards"),
             Arguments.of(listOf(Location(Point(1, 2), South)), Location(Point(1, 1), South), "B", "go backwards"),
-            Arguments.of(listOf(Location(Point(0, 1), North), Location(Point(0, 2), North)), Location(Point(0, 0), North), "FF", "turning front"),
-            Arguments.of(listOf(Location(Point(0, 0), West), Location(Point(0, 0), South)), Location(Point(0, 0), North), "LL", "turning left"),
-                Arguments.of(listOf(Location(Point(0, 0), East), Location(Point(0, 0), South)), Location(Point(0, 0), North), "RR", "turning right"),
+            Arguments.of(
+                listOf(Location(Point(0, 1), North), Location(Point(0, 2), North)),
+                Location(Point(0, 0), North),
+                "FF",
+                "turning front"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 0), West), Location(Point(0, 0), South)),
+                Location(Point(0, 0), North),
+                "LL",
+                "turning left"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 0), East), Location(Point(0, 0), South)),
+                Location(Point(0, 0), North),
+                "RR",
+                "turning right"
+            ),
         )
     }
 }
@@ -57,22 +74,118 @@ class CommandCalculationProviderAlt : ArgumentsProvider {
 class CommandCalculationProviderForEdges : ArgumentsProvider {
     override fun provideArguments(p0: ExtensionContext?): Stream<out Arguments> {
         return Stream.of(
-            Arguments.of(listOf(Location(Point(9, 0), West)), Location(Point(0, 0), West), "F", PlanetSize(10, 10), "turning front"),
-            Arguments.of(listOf(Location(Point(19, 0), West)), Location(Point(0, 0), West), "F", PlanetSize(20, 10), "turning front"),
-            Arguments.of(listOf(Location(Point(0, 0), East)), Location(Point(9, 0), East), "F", PlanetSize(10, 10), "turning front"),
-            Arguments.of(listOf(Location(Point(0, 0), East)), Location(Point(19, 0), East), "F", PlanetSize(20, 10), "turning front"),
-            Arguments.of(listOf(Location(Point(0, 0), North)), Location(Point(0, 9), North), "F", PlanetSize(10, 10), "turning front"),
-            Arguments.of(listOf(Location(Point(0, 0), North)), Location(Point(0, 19), North), "F", PlanetSize(10, 20), "turning front"),
-            Arguments.of(listOf(Location(Point(0, 9), South)), Location(Point(0, 0), South), "F", PlanetSize(10, 10), "turning front"),
-            Arguments.of(listOf(Location(Point(0, 19), South)), Location(Point(0, 0), South), "F", PlanetSize(10, 20), "turning front"),
-            Arguments.of(listOf(Location(Point(0, 9), North)), Location(Point(0, 0), North), "B", PlanetSize(10, 10), "go backwards"),
-            Arguments.of(listOf(Location(Point(0, 19), North)), Location(Point(0, 0), North), "B", PlanetSize(10, 20), "go backwards"),
-            Arguments.of(listOf(Location(Point(9, 0), East)), Location(Point(0, 0), East), "B", PlanetSize(10, 10), "go backwards"),
-            Arguments.of(listOf(Location(Point(19, 0), East)), Location(Point(0, 0), East), "B", PlanetSize(20, 10), "go backwards"),
-            Arguments.of(listOf(Location(Point(0, 0), West)), Location(Point(9, 0), West), "B", PlanetSize(10, 10), "go backwards"),
-            Arguments.of(listOf(Location(Point(0, 0), West)), Location(Point(19, 0), West), "B", PlanetSize(20, 10), "go backwards"),
-            Arguments.of(listOf(Location(Point(0, 0), South)), Location(Point(0, 9), South), "B", PlanetSize(10, 10), "go backwards"),
-            Arguments.of(listOf(Location(Point(0, 0), South)), Location(Point(0, 19), South), "B", PlanetSize(10, 20), "go backwards"),
+            Arguments.of(
+                listOf(Location(Point(9, 0), West)),
+                Location(Point(0, 0), West),
+                "F",
+                PlanetSize(10, 10),
+                "turning front"
+            ),
+            Arguments.of(
+                listOf(Location(Point(19, 0), West)),
+                Location(Point(0, 0), West),
+                "F",
+                PlanetSize(20, 10),
+                "turning front"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 0), East)),
+                Location(Point(9, 0), East),
+                "F",
+                PlanetSize(10, 10),
+                "turning front"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 0), East)),
+                Location(Point(19, 0), East),
+                "F",
+                PlanetSize(20, 10),
+                "turning front"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 0), North)),
+                Location(Point(0, 9), North),
+                "F",
+                PlanetSize(10, 10),
+                "turning front"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 0), North)),
+                Location(Point(0, 19), North),
+                "F",
+                PlanetSize(10, 20),
+                "turning front"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 9), South)),
+                Location(Point(0, 0), South),
+                "F",
+                PlanetSize(10, 10),
+                "turning front"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 19), South)),
+                Location(Point(0, 0), South),
+                "F",
+                PlanetSize(10, 20),
+                "turning front"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 9), North)),
+                Location(Point(0, 0), North),
+                "B",
+                PlanetSize(10, 10),
+                "go backwards"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 19), North)),
+                Location(Point(0, 0), North),
+                "B",
+                PlanetSize(10, 20),
+                "go backwards"
+            ),
+            Arguments.of(
+                listOf(Location(Point(9, 0), East)),
+                Location(Point(0, 0), East),
+                "B",
+                PlanetSize(10, 10),
+                "go backwards"
+            ),
+            Arguments.of(
+                listOf(Location(Point(19, 0), East)),
+                Location(Point(0, 0), East),
+                "B",
+                PlanetSize(20, 10),
+                "go backwards"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 0), West)),
+                Location(Point(9, 0), West),
+                "B",
+                PlanetSize(10, 10),
+                "go backwards"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 0), West)),
+                Location(Point(19, 0), West),
+                "B",
+                PlanetSize(20, 10),
+                "go backwards"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 0), South)),
+                Location(Point(0, 9), South),
+                "B",
+                PlanetSize(10, 10),
+                "go backwards"
+            ),
+            Arguments.of(
+                listOf(Location(Point(0, 0), South)),
+                Location(Point(0, 19), South),
+                "B",
+                PlanetSize(10, 20),
+                "go backwards"
+            ),
         )
     }
 }
@@ -89,13 +202,23 @@ class CommandShould {
         Assertions.assertEquals(expectedPath, result)
     }
 
-    @ParameterizedTest(name = "calculate {4} from give current location when source direction is {1}")
-    @ArgumentsSource(CommandCalculationProviderForEdges::class)
-    fun `calculate path over edge`(expectedPath: Path, sourceLocation: Location, commandValue: String, planetSize: PlanetSize, commandName: String) {
-        val command = Command(commandValue)
-        val result = command.calculatePathOver(sourceLocation, planetSize)
+    @Nested
+    @DisplayName("with edges")
+    inner class WithEdges {
+        @ParameterizedTest(name = "calculate {4} from give current location when source direction is {1}")
+        @ArgumentsSource(CommandCalculationProviderForEdges::class)
+        fun `calculate path over edge`(
+            expectedPath: Path,
+            sourceLocation: Location,
+            commandValue: String,
+            planetSize: PlanetSize,
+            commandName: String
+        ) {
+            val command = Command(commandValue)
+            val result = command.calculatePathOver(sourceLocation, planetSize)
 
-        Assertions.assertEquals(expectedPath, result)
+            Assertions.assertEquals(expectedPath, result)
+        }
+
     }
-
 }
